a2436b155a1b203055e54b4eb3d77bb7395d2c73
Mike Breen <hardbap@gmail.com>
2012-08-29 15:22:09 -0400

    Rename .locate => .locate_ports.

--- lib/wall_e/serial_snoop.rb
+++ lib/wall_e/serial_snoop.rb
@@ -5,17 +5,14 @@ module WallE
   module SerialSnoop
     include Logger
 
-    def locate
+    def locate_ports
       ports = Dir['/dev/*'].grep(/usb|acm/)
       board = nil
-
       if ports.any?
         what = 'serial port'
         what << 's' unless ports.one?
 
         info "Found possible #{what} #{ports}"
-
-        # TODO refactor to own method
         ports.each do |port|
           begin
             info "Connecting to #{port}..."
@@ -27,7 +24,6 @@ module WallE
             board = nil
           end
         end
-
       else
         error 'Error: No USB devices detected'
       end
--- test/serial_snoop_test.rb
+++ test/serial_snoop_test.rb
@@ -12,20 +12,20 @@ class SerialSnoopTest < MiniTest::Unit::TestCase
 
   def test_no_port_available
     Dir.stub :[], [] do
-      assert_nil Dummy.locate
+      assert_nil Dummy.locate_ports
     end
   end
 
   def test_cannot_connect_to_found_port
     Dir.stub :[], ['tty.usbmodemfd13131'] do
-      assert_nil Dummy.locate
+      assert_nil Dummy.locate_ports
     end
   end
 
   def test_connect_to_found_port
     Dir.stub :[], ['tty.usbmodemfd13131'] do
       Firmata::Board.stub :new, Dummy.new do
-        refute_nil Dummy.locate
+        refute_nil Dummy.locate_ports
       end
     end
   end
