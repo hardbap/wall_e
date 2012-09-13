65f87d576580c91f16104119c1852f03a3acdb12
Mike Breen <hardbap@gmail.com>
2012-09-13 16:23:45 -0400

    Add range info for Hitec servo.

--- cookbook/Servo/sweep.rb
+++ cookbook/Servo/sweep.rb
@@ -4,9 +4,11 @@ require 'wall_e'
 WallE::Assembler.build do
   # This example uses a Sparkfun's "Medium" servo.
   # (DGServo S05NF STD http://www.sparkfun.com/products/10333)
-  #
   # Using the range 15..170 appears to be the sweet spot for this servo.
+  #
   # YMMV especially if you are using a different servo.
+  #
+  # The Hitec HS-322 (http://amzn.com/B0006O3XEA) has a range of 2..180.
   servo = Servo(9, range: 15..170)
   servo.min
 
